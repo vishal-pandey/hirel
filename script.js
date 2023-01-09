4d476123ae608c8ab1c25c19e3398024f73852db
vishal-pandey <contact@vishalpandey.co.in>
2023-01-09 23:27:40 +0530

    udpates

--- script.js
+++ script.js
@@ -3,33 +3,65 @@ window.onload = () =>{
   let name = document.querySelector('input[name="name"]')
   let email = document.querySelector('input[name="email"]')
   let mobile = document.querySelector('input[name="mobile"]')
-                      
-
+  let password = document.querySelector('input[name="password"]')
+  
+  let referralButton = document.querySelector(".referral-submit")
+  let loginButton = document.querySelector(".login-button");
 
   
-  document.querySelector(".referral-submit").onclick = ()=>{
-    let errStr = ""
-    if(name.value == "") {
-      errStr += "All Fields are mandatory <br>"
-    } else {
-      errStr += ""
+  if(referralButton) {
+    referralButton.onclick = ()=>{
+      let errStr = ""
+      if(name.value == "") {
+        errStr += "All Fields are mandatory <br>"
+      } else {
+        errStr += ""
+      }
+  
+      if(!validateEmail(email.value)) {
+        errStr += "Please enter a valid email id <br>"
+      } else {
+        errStr += ""
+      }
+  
+      if(!validateMobile(mobile.value)) {
+        errStr += "Please enter 10 digit mobile number <br>";
+      } else {
+        errStr += ""
+      }
+      // if(errStr != ""){
+        document.querySelector(".error").innerHTML = errStr
+      // }
     }
-
-    if(!validateEmail(email.value)) {
-      errStr += "Please enter a valid email id <br>"
-    } else {
-      errStr += ""
-    }
-
-    if(!validateMobile(mobile.value)) {
-      errStr += "Please enter 10 digit mobile number <br>";
-    } else {
-      errStr += ""
-    }
-    // if(errStr != ""){
-      document.querySelector(".error").innerHTML = errStr
-    // }
   }
+
+  if(loginButton) {
+    loginButton.onclick = ()=>{
+      console.log("Here we are")
+      let errStr = ""
+  
+      if(!validateEmail(email.value)) {
+        errStr += "Please enter a valid email id <br>"
+      } else {
+        errStr += ""
+      }
+      
+      if(password.value == "") {
+        errStr += "Please enter password <br>"
+      } else {
+        errStr += ""
+      }
+      
+      document.querySelector(".error").innerHTML = errStr
+
+      if(errStr == "") {
+        window.location.href = "./auth.html"
+      }
+      // }
+    }    
+  }
+
+  
 }
 
 
