d213b293d88706789fa8bb3e14bd4784bb883ada
Vishal Pandey <contact@vishalpandey.co.in>
2023-07-01 10:35:26 +0000

    feature: header fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@
     $(window).on('scroll', function () {
       var scroll = $(window).scrollTop();
       if (scroll < 400) {
-        $(".header-sticky").removeClass("sticky-bar");
-        $('#back-top').fadeOut(500);
+        // $(".header-sticky").removeClass("sticky-bar");
+        // $('#back-top').fadeOut(500);
       } else {
         $(".header-sticky").addClass("sticky-bar");
         $('#back-top').fadeIn(500);
